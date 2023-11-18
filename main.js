e9f4c9253507a90b4c363a0ef82412e40429c38e
balascobaco <balascobaco@gmail.com>
2023-11-18 11:14:39 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function goToWhatsapp() {
+	window.location.href =
+		'https://api.whatsapp.com/send?phone=553599750666&text=Oi,%20Caio!%20Acabei%20de%20acessar%20o%20site%20da%20Wetag%20e%20gostaria%20de%20saber%20mais%20sobre%20os%20seus%20servi%C3%A7os!';
+}
+
+function topFunction() {
+	window.scrollTo({ top: 0, behavior: 'smooth' });
+}
